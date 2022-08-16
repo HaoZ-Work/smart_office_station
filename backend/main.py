aab4f26030e21158b25ad90a612757d695a87f67
Hao.Z <Hao.Z@outlook.de>
2022-08-16 11:31:19 +0200

    add api to get the last record

--- backend/main.py
+++ backend/main.py
@@ -6,6 +6,9 @@ from pydantic import BaseModel
 import pymongo
 import json
 
+DB_CLIENT_ADDRESS="mongodb://localhost:27017/"
+
+
 class Item(BaseModel):
     name:str
     temperature:float
@@ -16,11 +19,22 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message":"Hello world"}
+
 @app.post("/dht22_data/")
 async def receive_temperature(item:Item):
-    client = pymongo.MongoClient("mongodb://localhost:27017/")
+    client = pymongo.MongoClient(DB_CLIENT_ADDRESS)
     dht_db = client["dht22"]
     dht_table = dht_db['station01']
     dht_table.insert_one(jsonable_encoder(item))
     
     return item
+
+@app.get("/dht22_data/get_last/")
+async def query_last():
+    client = pymongo.MongoClient(DB_CLIENT_ADDRESS)
+    dht_db = client["dht22"]
+    dht_table = dht_db['station01']
+    last_record = dht_table.find_one(sort=[( '_id', pymongo.DESCENDING )])
+    last_record.pop('_id')
+
+    return last_record
