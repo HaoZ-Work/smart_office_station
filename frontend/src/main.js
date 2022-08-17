9454ab9b18090b9495ebc5a2f9701c344d92ebf0
Hao.Z <Hao.Z@outlook.de>
2022-08-17 12:52:22 +0200

    add proxy setting for Access-control issue

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,4 +4,11 @@ import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import "bootstrap"
 
+
+import axios from 'axios'
+// import Vue from 'vue'
+
+// Vue.prototype.$axios = axios
+axios.defaults.baseURL = '/api'
+
 createApp(App).mount('#app')
