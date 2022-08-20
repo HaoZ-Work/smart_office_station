0afb1b05240eb7a347b9cabb3327f955e29f7cb4
Hao.Z <Hao.Z@outlook.de>
2022-08-20 11:35:05 +0200

    refactor the path of api

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Path
 from fastapi.encoders import jsonable_encoder
+from fastapi.middleware.cors import CORSMiddleware
+from data_model import *
 
 from typing import Union
 from pydantic import BaseModel
@@ -9,31 +11,61 @@ import json
 DB_CLIENT_ADDRESS="mongodb://localhost:27017/"
 
 
-class Item(BaseModel):
-    name:str
-    temperature:float
-    humidity:float
 
 app = FastAPI()
+
+origins = [
+ 
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 async def root():
     return {"message":"Hello world"}
 
-@app.post("/dht22_data/")
-async def receive_temperature(item:Item):
-    client = pymongo.MongoClient(DB_CLIENT_ADDRESS)
-    dht_db = client["dht22"]
-    dht_table = dht_db['station01']
-    dht_table.insert_one(jsonable_encoder(item))
-    
-    return item
+@app.post("/dht22/{userid}")
 
-@app.get("/dht22_data/get_last/")
-async def query_last():
+async def receive_temperature(
+    data: DHT22,
+   
+    userid:str=Path(...,title='user id',description="The id of user") 
+    ):
+    '''
+    Receive the dht22 data from smart station and write the data into the corresponding data base.
+    '''
+
+    client = pymongo.MongoClient(DB_CLIENT_ADDRESS)
+    dht_db = client['dht22']
+    
+    dht_table = dht_db[userid]
+    dht_table.insert_one(jsonable_encoder(data))
+    
+    return data
+
+@app.get("/dht22/{userid}/")
+async def query_last(
+    userid:str=Path(...,title='user id',description="The id of user") 
+):
+    '''
+    Get the lastest one recording of the data base.
+
+    Return:
+        last_record: a recording in dict() type
+    
+    '''
     client = pymongo.MongoClient(DB_CLIENT_ADDRESS)
     dht_db = client["dht22"]
-    dht_table = dht_db['station01']
+    dht_table = dht_db[userid]
     last_record = dht_table.find_one(sort=[( '_id', pymongo.DESCENDING )])
     last_record.pop('_id')
 
