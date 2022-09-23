a933903040cc5fd6149f154cddf832b0499d4a1c
root <root@DESKTOP-CR8HHVN.localdomain>
2022-09-23 10:12:59 +0200

    add settings for frontend

--- smartstation_backend/smartstation_backend/settings.py
+++ smartstation_backend/smartstation_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,10 @@ SECRET_KEY = 'django-insecure-e$9i&c@ya^83o6(z4%291+bvdowcxluzfv3uj2jjofy-ziyts&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '172.29.165.50',
+    '127.0.0.1'
+]
 
 
 # Application definition
@@ -43,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -55,7 +60,8 @@ ROOT_URLCONF = 'smartstation_backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        # 'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': ['frontend/dist',BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -124,3 +130,19 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # LOGIN_REDIRECT_URL = 'index' # URL redirecting after a successful authentication
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "frontend/dist/static"),
+# ]
+STATICFILES_DIRS = [
+    # "/home/dtdysh/smart_office_station/smartstation_backend/frontend/dist/js",
+    # os.path.join(BASE_DIR, "frontend/dist/js"),
+    # os.path.join(BASE_DIR, "frontend/dist/css"),
+    os.path.join(BASE_DIR, "frontend/dist/static"),
+
+
+]
+
+CORS_ORIGIN_ALLOW_ALL = True
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1']
+
+
--- smartstation_backend/smartstation_backend/urls.py
+++ smartstation_backend/smartstation_backend/urls.py
@@ -17,7 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include, path
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('userpages/', include('userpages.urls')),
+
 ]
