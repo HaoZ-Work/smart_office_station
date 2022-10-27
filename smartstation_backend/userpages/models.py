d3fc6d92be88281f71006be44b18961f4201ef41
Hao.Z <Hao.Z@outlook.de>
2022-10-27 09:29:08 +0200

    chore:remove unnecessary files

--- smartstation_backend/userpages/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-# class UserModel(models.Model):
-#     userid = models.IntegerField()
-#     def __str__(self) -> str:
-#         return self.userid
