022afff67c11ab33f6e12e66a0f6a6191605f5aa
Hao.Z <Hao.Z@outlook.de>
2023-03-16 10:57:00 +0100

    chore:delete the unnecessay files

--- C_version/pomodoro.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <Arduino.h>
-
-void pomodoro_setup();
-void pomodoro_update();
-String pomodoro_get_html();
--- C_version/weather.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <Arduino.h>
-
-void weather_setup();
-void weather_update();
-String weather_get_html();
