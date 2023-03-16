a80c834b33c2e44f98716bd688be52d956d92610
Hao.Z <Hao.Z@outlook.de>
2023-03-16 10:57:00 +0100

    chore:delete the unnecessay files

--- C_version/attention.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <Arduino.h>
-
-void attention_setup();
-void attention_update();
-String attention_get_html();
--- C_version/html.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <Arduino.h>
-
-String get_website_head();
-String get_website_tail();
-String get_window(String svg_image_link, String content);
